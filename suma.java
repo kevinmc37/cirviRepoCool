737caf5da394d4a831afb4896999b97214a70fc4
Kevin Morales <kevinmc_97@hotmail.es>
2017-09-27 16:26:33 +0200

    s'afeigeix el arxiou de la suma en catala

--- suma.java
+++ suma.java
@@ -2,8 +2,8 @@
 int numero1 = 0;
 int numero2 = 0;
 
-System.out.println("Introduce el primer número:");
+System.out.println("Introdueix el numero1:");
 numero1 = reader.nextInt();
 
-System.out.println("Introduce el segundo número:");
+System.out.println("Introduiex wle num2:");
 numero2 = reader.nextInt();
