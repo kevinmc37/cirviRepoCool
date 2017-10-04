a4da84d847777bdd04bf264160c6825bacb3f48e
Kevin Morales <kevinmc_97@hotmail.es>
2017-10-04 16:49:17 +0200

    holaogoasn
    trhythth puja hello y suma

--- helloWorld.php
+++ helloWorld.php
@@ -1,1 +1,3 @@
 kuerbruibgr
+
+grgrogribgirbg
---
+++ suma.java
@@ -0,0 +1,9 @@
+
+int numero1 = 0;
+int numero2 = 0;
+
+System.out.println("Introdueix el numero1:");
+numero1 = reader.nextInt();
+
+System.out.println("Introduu6yuyuyuum2:");
+numero2 = reader.nextInt();
